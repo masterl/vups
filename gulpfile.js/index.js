1eb327f1d5b14fb7af5f03a1c16ed66595bfa70a
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-06-09 16:20:57 -0700

    Added plugins css bundling

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -5,10 +5,23 @@ const compile = {
   index:        require('./compile-index')
 };
 
+const npm = {
+  css: require('./npm-css')
+};
+
 const compile_tasks = [];
+const npm_tasks = [];
 
 for (const key in compile) {
   compile_tasks.push(compile[key]);
+
+  exports[`compile:${key}`] = compile[key];
+}
+
+for (const key in npm) {
+  npm_tasks.push(npm[key]);
+
+  exports[`npm:${key}`] = npm[key];
 }
 
 function default_task (cb) {
@@ -22,6 +35,7 @@ for (const key in compile) {
 }
 
 exports.default = series(
+  parallel(...npm_tasks),
   parallel(...compile_tasks),
   default_task
 );
